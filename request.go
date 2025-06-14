3a167b0030c892ed840da34348ca351cddfc9ac5
jaron <jaron@jaronnie.com>
2025-06-14 19:44:28 +0800

    feat(restc): refactor param

--- request.go
+++ request.go
@@ -107,6 +107,8 @@ func (r *Request) AddHeader(key, value string) {
 	if r.headers == nil {
 		r.headers = http.Header{}
 	}
+	r.c.lock.Lock()
+	defer r.c.lock.Unlock()
 	r.headers.Add(key, value)
 }
 
